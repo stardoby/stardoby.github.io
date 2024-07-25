0e0eff98d8219edf12aef295037f23fc68a2635c
Star Doby <stardoby@stanford.edu>
2024-07-25 13:04:32 -0700

    figure out pages

--- portfolio/my-portfolio/src/app/loading/page.tsx
+++ portfolio/my-portfolio/src/app/loading/page.tsx
@@ -3,7 +3,7 @@
 export default function Loading() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>hello world!</p>
+      <p>this is the where the loading begins!</p>
     </main>
   );
 }
