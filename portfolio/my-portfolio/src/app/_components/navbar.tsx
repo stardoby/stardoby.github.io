78e9fbbd6a9b232b71bf0474e3264c1efeb967fa
Star Doby <stardoby@stanford.edu>
2024-08-16 16:22:50 -0700

    create navbar

---
+++ portfolio/my-portfolio/src/app/_components/navbar.tsx
@@ -0,0 +1,41 @@
+'use client';
+import soundOn from '../assets/sound-on.png';
+import soundOff from '../assets/sound-off.png';
+import Hamburger from '../assets/hamburger.png';
+import Image from 'next/image';
+import React, {useEffect, useState} from 'react';
+
+export default function NavBar() {
+
+    const [soundToggle, setSoundToggle] = useState(true);
+
+    function toggleSound() {
+        if (soundToggle) {
+            setSoundToggle(false);
+        } else {
+            setSoundToggle(true);
+        }
+    }
+
+    return (
+        <div className="w-screen h-fit flex flex-row justify-between py-7 px-14">
+            <div>{<Image id='hamburger' 
+            src={Hamburger} 
+            width={41} 
+            height={41} 
+            alt="hamburger icon"></Image>}</div>
+            <button id="sound-button" onClick={toggleSound}>{soundToggle ? 
+                <Image id='sound-on-icon' 
+                src={soundOn} 
+                width={77} 
+                height={41} 
+                alt="sound on speaker"></Image> :
+                <Image id='sound-off-icon' 
+                src={soundOff} 
+                width={77} 
+                height={41} 
+                alt="sound off speaker"></Image>}</button>
+        </div>
+    );
+}
+
