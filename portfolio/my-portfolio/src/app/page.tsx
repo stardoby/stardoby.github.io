5e9f92130c363d979e524cca8e90ea0ab3c2ebf7
Star Doby <stardoby@stanford.edu>
2024-08-18 19:40:10 -0700

    prepare to add elements to home

--- portfolio/my-portfolio/src/app/page.tsx
+++ portfolio/my-portfolio/src/app/page.tsx
@@ -5,6 +5,11 @@ import NavBar from './_components/navbar';
 export default function Home() {
   
   return (
-   <main><NavBar></NavBar></main>
+   <main><NavBar></NavBar>
+   <div id="main-container" className="flex flex-row w-screen h-screen">
+      <div id="hero-container" className='h-full w-3/6 bg-violet-300'></div>
+      <div id="name-title" className=' h-full w-3/6 bg-blue-300'></div>
+   </div>
+   </main>
   );
 }
