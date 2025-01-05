6e6adc31c5cc8ee3441386243c2f27534e83f456
Star Doby <stardoby@stanford.edu>
2025-01-04 18:00:05 -0800

    add file

--- portfolio/my-portfolio/webpack.config.js
+++ portfolio/my-portfolio/webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: ['.tsx', '.ts', '.js', '.pdf'],
   },
   output: {
     filename: 'bundle.js',
